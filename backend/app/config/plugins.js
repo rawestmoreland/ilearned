aeedead8d443a24049c4d822f13c21035cbbea4b
rawestmoreland <rawestmoreland@gmail.com>
2022-06-04 07:52:41 -0500

    gql config

--- backend/app/config/plugins.js
+++ backend/app/config/plugins.js
@@ -36,7 +36,7 @@ module.exports = ({ env }) => ({
     config: {
       endpoint: "/graphql",
       shadowCRUD: true,
-      playgroundAlways: false,
+      playgroundAlways: true,
       depthLimit: 7,
       amountLimit: 100,
       apolloServer: {
