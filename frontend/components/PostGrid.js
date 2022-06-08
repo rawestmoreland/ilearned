a36d303b6edb9c8edd0c1a585dfee334ede55b95
rawestmoreland <rawestmoreland@gmail.com>
2022-06-08 14:17:42 -0500

    footer

--- frontend/components/PostGrid.js
+++ frontend/components/PostGrid.js
@@ -2,7 +2,7 @@ import PostCard from './PostCard'
 
 const PostGrid = ({ posts, marginTop = 16 }) => {
 	return (
-		<div className={`grid grid-cols-post-grid gap-8 mb-16 mt-${marginTop}`}>
+		<div className={`grid grid-cols-post-grid gap-8 pb-12 mt-${marginTop}`}>
 			{posts.map((post) => {
 				return (
 					<div key={post.id} className='col-span-1'>
