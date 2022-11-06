fdcfb2fda5f073b8a5e67179548676e9681ea5f4
Richard Westmoreland <rawestmoreland@gmail.com>
2022-11-05 23:06:23 -0500

    style(frontend): fix top margin on post card grid

--- frontend/components/PostGrid.js
+++ frontend/components/PostGrid.js
@@ -1,8 +1,8 @@
 import PostCard from './PostCard';
 
-const PostGrid = ({ posts, marginTop = 16 }) => {
+const PostGrid = ({ posts, marginTop = 8 }) => {
   return (
-    <div className={`grid grid-cols-post-grid gap-8 pb-12 md:mt-${marginTop} mt-32`}>
+    <div className={`grid grid-cols-post-grid gap-8 pb-12 md:mt-${marginTop} mt-24`}>
       {posts.map(post => {
         return (
           <div key={post.id} className="col-span-1">
